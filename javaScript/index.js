0d09af7318909904821607ee9dd45c727e471154
adeniran <adeniranquwam001@gmail.com>
2023-01-15 00:41:13 +0100

    first commit

---
+++ javaScript/index.js
@@ -0,0 +1,32 @@
+let btn = document.querySelector(".btn");
+let zero = document.querySelector(".zero");
+let pp = document.querySelector(".pp");
+
+let zero2 = 0;
+// console.log(zero2);
+// zero.textContent=zero2
+// console.log(zero);
+btn.addEventListener("click", () => {
+  zero2++;
+  console.log(zero2);
+
+  zero.textContent=zero2
+
+  for(zero2=0; zero2>0;)
+
+    break;
+
+
+
+    console.log(zero2);
+
+});
+
+
+// zero2++
+//     zero.textContent=zero2
+
+//     console.log(zero2);
+//     for(zero=0; zero>0;)
+//     break;
+//     zero.textContent=zero2
